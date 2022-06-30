ed953b5faf84c52f5c9905067d23172fb25cbe09
Stepan S <dumn2011@gmail.com>
2022-06-30 11:05:47 +0900

    also

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3033;
 const routes = require("./lib/routes")
 const cors = require("cors");
 const bodyParser = require("body-parser")
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
