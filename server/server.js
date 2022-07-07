00361508783e34edabc639b74bcaf314c86beba7
Stepan S <dumn2011@gmail.com>
2022-07-07 18:00:17 +0900

    for dockerfile changes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3033;
+const host = "0.0.0.0"
 const routes = require("./lib/routes")
 const cors = require("cors");
 const bodyParser = require("body-parser")
@@ -18,8 +19,8 @@ app.post("/api/create", routes.createNewEvent)
 app.delete("/api/delete", routes.deleteEvent)
 
 
-app.listen(port, () => {
+app.listen(port, host, () => {
 	console.clear()
-	console.log(`Server working on http://localhost:${port}`)
+	console.log(`Server working on http://${host}:${port}`)
 })
 
